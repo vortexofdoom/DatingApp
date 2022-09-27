6125909c5e699c686ba5512c88190af67b315a19
Daniel Larson <vortexofdoom97@gmail.com>
2022-09-27 12:11:09 -0500

    Added UI cards and details

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
@@ -8,7 +9,7 @@ import { User } from '../_models/user';
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
